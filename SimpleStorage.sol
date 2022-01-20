c321b55df62aa5f95e75a339457417932502ca58
PinkPigmyPuff <67124610+PinkPigmyPuff@users.noreply.github.com>
2022-01-20 11:38:38 -0800

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    uint256 public favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+}

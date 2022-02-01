f95fc3956ce2836411b9521ad57f107f3836cbca
PinkPigmyPuff <67124610+PinkPigmyPuff@users.noreply.github.com>
2022-01-31 21:20:42 -0800

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,9 +2,32 @@ pragma solidity ^0.6.0;
 
 contract SimpleStorage {
 
-    uint256 public favoriteNumber;
+    // this will get initialized to 0
+    uint256 favoriteNumber;
+
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    //this is a view function, which doesn't require a transaction on the blockchain (blue button)
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    
+    //an array is technically an array of bytes,
+    //so when we use a parameter that is going to be a string for one of our functions, we need to call it memory or storage
+    //memory means, after execution, delete this variable
+    //storage means keep it forever
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
